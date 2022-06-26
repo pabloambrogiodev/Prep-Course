479680c709626257388526895574f908ba42c05c
pabloambrogiodev <pablo.developer.7@gmail.com>
2022-06-26 15:26:55 -0300

    Se agrego archivo text.text en la carpeta homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -112,7 +112,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if( num % 2 == 0 ) {
+  if( num % 2 === 0 ) {
     return true;
   } else {
     return false;
@@ -124,7 +124,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if( num % 2 !== 0 ){
+  if( num % 2 === 1 ){
     return true;
   } else {
     return false;
@@ -135,19 +135,19 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return num ** 2;
+  return Math.pow( num, 2 );
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return num ** 3;
+  return Math.pow( num, 3 );
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return num ** exponent;
+  return Math.pow( num, exponent );
 }
 
 function redondearNumero(num) {
